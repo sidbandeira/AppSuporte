59b6416b7dc7b9fa86cca761da7fbdceb64e9a2b
sidbandeira <sid-bandeira@hotmail.com>
2017-04-07 13:45:29 -0300

    Commit 070417

---
+++ app/src/main/java/com/bandeira/sidnei/appleitor/CadastraColetaActivity.java
@@ -0,0 +1,13 @@
+package com.bandeira.sidnei.appleitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastraColetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastra_coleta);
+    }
+}
