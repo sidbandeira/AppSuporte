af8f5bf2ddc8bb732e56d839f740d1560e0d58af
sidbandeira <sid-bandeira@hotmail.com>
2017-04-18 15:02:34 -0300

    Commit 180417_2

--- app/src/main/java/com/bandeira/sidnei/appleitor/CadastraColetaActivity.java
+++ app/src/main/java/com/bandeira/sidnei/appleitor/CadastraColetaActivity.java
@@ -6,22 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import google.zxing.integration.android.IntentIntegrator;
 import google.zxing.integration.android.IntentResult;
 
 public class CadastraColetaActivity extends AppCompatActivity {
-
+    public static String delimitador = "";
+    private EditText edtDescricao;
+    //private EditText edtDelimitador;
+    private Button btConfirma;
+    private Button btCancela;
+    private Spinner spDelimitador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastra_coleta);
 
+        edtDescricao = (EditText)findViewById(R.id.edtDescricao);
+        //edtDelimitador = (EditText)findViewById(R.id.edtDelimitador);
+        btConfirma = (Button)findViewById(R.id.btConfirma);
+        btCancela = (Button)findViewById(R.id.btCancela);
+        spDelimitador = (Spinner) findViewById(R.id.spDelimitador);
+
     }
 
 
     public void btCofirma(View view) {
+
+        //delimitador = edtDelimitador.getText().toString();
+        delimitador = (String)spDelimitador.getSelectedItem();
+        Toast.makeText(this,delimitador,Toast.LENGTH_SHORT).show();
         Intent it = new Intent(this, ColetaActivity.class);
         startActivity(it);
     }
