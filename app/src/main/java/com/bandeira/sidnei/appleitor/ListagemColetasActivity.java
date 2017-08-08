a7f47cc2387c13fe57538455da27dcb1911f24f5
sidbandeira <sid-bandeira@hotmail.com>
2017-08-08 07:47:38 -0300

    Implementado ação para os botões (excluir,voltar) da mensagem da listagem de coletas.

--- app/src/main/java/com/bandeira/sidnei/appleitor/ListagemColetasActivity.java
+++ app/src/main/java/com/bandeira/sidnei/appleitor/ListagemColetasActivity.java
@@ -1,11 +1,16 @@
 package com.bandeira.sidnei.appleitor;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.bandeira.sidnei.appleitor.Adaptadores.AdapterListagemColeta;
 import com.bandeira.sidnei.appleitor.Classes.Coleta;
+import com.bandeira.sidnei.appleitor.Repositorio.ColetaItemRepositorio;
 import com.bandeira.sidnei.appleitor.Repositorio.ColetaRepositorio;
 
 import java.util.ArrayList;
@@ -14,20 +19,77 @@ import java.util.List;
 public class ListagemColetasActivity extends AppCompatActivity {
     private ListView listaColeta;
     List<Coleta> coleta = new ArrayList<Coleta>();
+    private Integer posicao = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listagem_coletas);
         listaColeta = (ListView)findViewById(R.id.listagemColetas);
-        listarCOletas();
+        listarColetas();
+
+        listaColeta.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                posicao = position;
+                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ListagemColetasActivity.this);
+
+                // SETA UM TITULO PARA O DIALOG
+                alertDialogBuilder.setTitle("Escolha a opção desejada");
+
+                // SETA UMA MENSAGEM PARA O DIALOG
+                alertDialogBuilder
+                        .setMessage("O que deseja fazer?")
+                        // BOTAO 1
+                        .setPositiveButton("Coletar",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                //COLOCAR AQUI A AÇÃO DO BOTAO
+                            }
+                        })
+
+                        // BOTAO 2
+                        .setNegativeButton("Excluir",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                listaColeta.setAdapter(null);
+                                excluirColeta((int) coleta.get( posicao)._id);
+                                finish();
+                            }
+                        })
+
+                        // BOTAO 3
+                        .setNeutralButton("Voltar",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                // CRIA O DIALOG
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // EXIBE O DIALOG
+                alertDialog.show();
+
+            }
+
+        });
     }
 
-    private void listarCOletas(){
+
+    private void listarColetas(){
         ColetaRepositorio rep = new ColetaRepositorio(this);
         coleta = rep.buscarColeta(null);
-
         listaColeta.setAdapter(new AdapterListagemColeta(this, (ArrayList<Coleta>) coleta ));
+    }
+
+    private void excluirColeta(Integer codigo){
+        // EXCLUI OS ITEMS DA COLETA
+        ColetaItemRepositorio repItem = new ColetaItemRepositorio(this);
+        repItem.excluirTodosItens(codigo);
+        // EXCLUI A COLETA
+        ColetaRepositorio rep = new ColetaRepositorio(this);
+        rep.excluir(codigo);
+
     }
 
 }
