bb6271d3484317d9501766c6306adf7997fc91bc
sidbandeira <sid-bandeira@hotmail.com>
2017-08-09 07:30:39 -0300

    Implementado o menu Excluir e  removido da tela de listagem a opção de excluir.

--- app/src/main/java/com/bandeira/sidnei/appleitor/ExcluirColetasActivity.java
+++ app/src/main/java/com/bandeira/sidnei/appleitor/ExcluirColetasActivity.java
@@ -1,23 +1,22 @@
 package com.bandeira.sidnei.appleitor;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.bandeira.sidnei.appleitor.Adaptadores.AdapterListagemColeta;
 import com.bandeira.sidnei.appleitor.Classes.Coleta;
+import com.bandeira.sidnei.appleitor.Repositorio.ColetaItemRepositorio;
 import com.bandeira.sidnei.appleitor.Repositorio.ColetaRepositorio;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListagemColetasActivity extends AppCompatActivity {
+public class ExcluirColetasActivity extends AppCompatActivity {
     private ListView listaColeta;
     List<Coleta> coleta = new ArrayList<Coleta>();
     private Integer posicao = 0;
@@ -25,16 +24,18 @@ public class ListagemColetasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listagem_coletas);
+        setContentView(R.layout.activity_excluir_coletas);
+
         listaColeta = (ListView)findViewById(R.id.listagemColetas);
         listarColetas();
+
 
         listaColeta.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 posicao = position;
-                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ListagemColetasActivity.this);
+                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ExcluirColetasActivity.this);
 
                 // SETA UM TITULO PARA O DIALOG
                 alertDialogBuilder.setTitle("Escolha a opção desejada");
@@ -42,15 +43,13 @@ public class ListagemColetasActivity extends AppCompatActivity {
                 // SETA UMA MENSAGEM PARA O DIALOG
                 alertDialogBuilder
                         .setMessage("O que deseja fazer?")
-                        // BOTAO 1
-                        .setPositiveButton("Coletar",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
 
-                                if ((int) coleta.get( posicao)._id <= 0) {
-                                    Toast.makeText(getApplicationContext(), "Erro ao selecionar uma coleta!", Toast.LENGTH_SHORT).show();
-                                }else{
-                                    continuarColeta((int) coleta.get( posicao)._id);
-                                }
+                        // BOTAO 1
+                        .setNegativeButton("Excluir",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                listaColeta.setAdapter(null);
+                                excluirColeta((int) coleta.get( posicao)._id);
+                                finish();
                             }
                         })
 
@@ -79,11 +78,13 @@ public class ListagemColetasActivity extends AppCompatActivity {
         listaColeta.setAdapter(new AdapterListagemColeta(this, (ArrayList<Coleta>) coleta ));
     }
 
-
-    private void continuarColeta(Integer codigo){
-        Intent it = new Intent(this, ColetaActivity.class);
-        it.putExtra("codigoColeta", String.valueOf( codigo));
-        startActivity(it);
+    private void excluirColeta(Integer codigo){
+        // EXCLUI OS ITEMS DA COLETA
+        ColetaItemRepositorio repItem = new ColetaItemRepositorio(this);
+        repItem.excluirTodosItens(codigo);
+        // EXCLUI A COLETA
+        ColetaRepositorio rep = new ColetaRepositorio(this);
+        rep.excluir(codigo);
     }
 
 }
