ccf45da5bd144afa8c41265f78a9266a43d929e9
sidbandeira <sid-bandeira@hotmail.com>
2018-08-28 15:33:44 -0300

    Adicionado as urls para acesso ao webservice.

--- app/src/main/java/com/bandeira/sidnei/appleitor/Classes/ProdutoHttp.java
+++ app/src/main/java/com/bandeira/sidnei/appleitor/Classes/ProdutoHttp.java
@@ -4,7 +4,8 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import org.json.JSONArray;
+import com.bandeira.sidnei.appleitor.PesquisarProdutosActivity;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -17,8 +18,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProdutoHttp {
-    public static final String PRODUTO_URL_JSON = "https://drive.google.com/open?id=1ANzr-zYTB2AH5WxnFX_pS2beSQSuB8HW";
-    //public static final String PRODUTO_URL_JSON = "http://sgestao.hol.es/ws/ProdutoWs.php?codempresa=";
+    //public static final String PRODUTO_URL_JSON = "http://10.0.2.2/arquivos/arquivoglobonovo.json"; CONEXAO LOCAL HOST
+    //public static final String PRODUTO_URL_JSON = "http://suporte-cloud.azurewebsites.net/api/produtoglobo"; CONEXAO EXTERNA
+    public static final String PRODUTO_URL_JSON = "http://192.168.200.23:8080/api/produto"; //CONEXAO INTERNA
 
     private static HttpURLConnection connectar(String urlArquivo) throws IOException {
         final int SEGUNDOS = 1000;
@@ -46,9 +48,9 @@ public class ProdutoHttp {
         try {
 
             //HttpURLConnection conexao = connectar(PRODUTO_URL_JSON + empresa);
-            HttpURLConnection conexao = connectar(PRODUTO_URL_JSON);
+            HttpURLConnection conexao = connectar(PRODUTO_URL_JSON + "/" + PesquisarProdutosActivity.codproduto);
             int resposta = conexao.getResponseCode();
-            if (resposta == HttpURLConnection.HTTP_OK) {
+            if (resposta == HttpURLConnection.HTTP_OK || resposta == HttpURLConnection.HTTP_CREATED) {
                 InputStream is = conexao.getInputStream();
                 JSONObject json = new JSONObject(bytesParaString(is));
                 return lerJsonProd(json);
@@ -59,25 +61,39 @@ public class ProdutoHttp {
         return null;
     }
 
-    public static List<Produto> lerJsonProd(JSONObject json) throws JSONException {
+    public static List<Produto> lerJsonProd(JSONObject json1) throws JSONException {
         List<Produto> listadeProduto = new ArrayList<Produto>();
         //OBJETO PAI DO JSON
-        JSONArray jsonProd = json.getJSONArray("produtos");
-        for (int i = 0; i < jsonProd.length(); i++) {
-            JSONObject jsonProduto = jsonProd.getJSONObject(i);
-            Produto prod = new Produto(
-                    jsonProduto.getInt("ProCodigo"),
-                    jsonProduto.getString("ProBarras"),
-                    jsonProduto.getString("ProNome"),
-                    jsonProduto.getString("ClasseFiscal"),
-                    jsonProduto.getString("Unidade"),
-                    jsonProduto.getDouble("SaldoTotal"),
-                    jsonProduto.getDouble("UltimoCusto"),
-                    jsonProduto.getDouble("PrecoVenda"),
-                    jsonProduto.getDouble("PrecoVendaLista"),
-                    jsonProduto.getString("LinkImagem"));
-            listadeProduto.add(prod);
-        }
+        //JSONArray jsonProd = json.getJSONArray("produtos");
+
+        Produto prod = new Produto(
+                json1.getInt("procodigo"),
+                json1.getString("probarras"),
+                json1.getString("pronome"),
+                "",
+                "",
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                "");
+        listadeProduto.add(prod);
+
+//        for (int i = 0; i < jsonProd.length(); i++) {
+//            JSONObject jsonProduto = jsonProd.getJSONObject(i);
+//            Produto prod = new Produto(
+//                    jsonProduto.getInt("ProCodigo"),
+//                    jsonProduto.getString("ProBarras"),
+//                    jsonProduto.getString("ProNome"),
+//                    jsonProduto.getString("ClasseFiscal"),
+//                    jsonProduto.getString("Unidade"),
+//                    jsonProduto.getDouble("SaldoTotal"),
+//                    jsonProduto.getDouble("UltimoCusto"),
+//                    jsonProduto.getDouble("PrecoVenda"),
+//                    jsonProduto.getDouble("PrecoVendaLista"),
+//                    jsonProduto.getString("LinkImagem"));
+//            listadeProduto.add(prod);
+//        }
         return listadeProduto;
     }
 
