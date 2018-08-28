31705317a721680a8e8a17d4dd4d42a35cc23e2a
sidbandeira <sid-bandeira@hotmail.com>
2018-08-28 15:36:12 -0300

    Adicionado activity pesquisarprodutos para passar o codigo do produto que deseja consultar

---
+++ app/src/main/java/com/bandeira/sidnei/appleitor/PesquisarProdutosActivity.java
@@ -0,0 +1,38 @@
+package com.bandeira.sidnei.appleitor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PesquisarProdutosActivity extends AppCompatActivity {
+    public static int codproduto = 0 ;
+    private EditText edtCodigoProduto;
+    private Button btPesquisar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesquisar_produtos);
+
+        edtCodigoProduto = (EditText)findViewById(R.id.edtCodigoProduto);
+        btPesquisar = (Button)findViewById(R.id.btPesquisar);
+    }
+
+    public void btPesquisar(View view) {
+        if (edtCodigoProduto.getText().toString().isEmpty()){
+
+            Toast.makeText(this,"Informe um produto válido!!",Toast.LENGTH_SHORT).show();
+            edtCodigoProduto.hasFocus();
+        }else{
+            codproduto = Integer.parseInt(edtCodigoProduto.getText().toString());
+            Intent it = new Intent(this, ProdutosActivity.class);
+            startActivity(it);
+        }
+
+    }
+}
