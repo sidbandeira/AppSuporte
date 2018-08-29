1e9639bbdbb8c60b7dbf7bc0affd561fe6c29798
sidbandeira <sid-bandeira@hotmail.com>
2018-08-29 17:56:48 -0300

    adicionado menu de configurações.

--- app/src/main/java/com/bandeira/sidnei/SuporteMobile/PrincipalActivity.java
+++ app/src/main/java/com/bandeira/sidnei/SuporteMobile/PrincipalActivity.java
@@ -3,6 +3,8 @@ package com.bandeira.sidnei.SuporteMobile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class PrincipalActivity extends AppCompatActivity {
@@ -17,5 +19,28 @@ public class PrincipalActivity extends AppCompatActivity {
         Intent it = new Intent(this, MenuPrincipalActivity.class);
         startActivity(it);
 
+    }
+
+    //chamada para o menu de configuração
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.configurar:
+                Intent it = new Intent(this, ConfiguracaoActivity.class);
+                startActivity(it);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
